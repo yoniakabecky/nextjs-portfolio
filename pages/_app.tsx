de89218e64b7f70a461ec181fe23b23521e259af
Yoni <46503638+yoniakabecky@users.noreply.github.com>
2021-04-29 16:11:31 -0700

    Change google fonts import method  (#3)
    * Add page title and description
    
    * Create social account section
    
    * Main contents
    
    * Add top navigation
    
    * Change embedding google font from link to import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,13 @@
 import { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import BasicMeta from "../components/meta/BasicMeta";
-import GoogleFontsMeta from "../components/meta/GoogleFontsMeta";
-import GlobalStyles from "../styles/globalStyles";
 import theme from "../styles/theme";
+import "../styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <BasicMeta />
-      <GoogleFontsMeta />
-
-      <GlobalStyles />
 
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
