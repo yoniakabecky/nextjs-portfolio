582ee27d08941f44e6e79fe697bd91b819dcc679
Yoni <yoniakabecky@gmail.com>
2021-05-17 18:25:01 -0700

    fetch projects data from prismicio

---
+++ pages/works/[slug].tsx
@@ -0,0 +1,52 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import { RichText } from "prismic-reactjs";
+import React, { ReactElement } from "react";
+import styled from "styled-components";
+import { getDocumentsByType, getProjectByUid } from "../../lib/prismicio";
+import { IProject } from "../../types";
+
+interface Props {
+  data: IProject;
+}
+
+export default function Project({ data }: Props): ReactElement {
+  return (
+    <Root>
+      <div>{RichText.render(data.hero[0].title)}</div>
+      <div>
+        <div>{RichText.render(data.about[0].title)}</div>
+        <div>{RichText.render(data.about[0].description)}</div>
+      </div>
+    </Root>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await getDocumentsByType("projects");
+
+  return {
+    paths: res.map((doc) => `/works/${doc.uid}`),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  if (params?.slug) {
+    const data = await getProjectByUid(params.slug as string);
+
+    return {
+      props: { data },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
+
+const Root = styled.main`
+  min-width: 100vw;
+  min-height: 100vh;
+  background-color: ${(props) => props.theme.colors.light};
+  color: ${(props) => props.theme.colors.dark};
+`;
