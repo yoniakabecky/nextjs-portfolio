e0c87c5322d3706e323f067a97ee839e86683f92
Yoni <yoniakabecky@gmail.com>
2021-06-06 23:11:54 -0700

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import { AnimateSharedLayout } from "framer-motion";
 import { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import BasicMeta from "@@/components/meta/BasicMeta";
+import Meta from "@@/components/meta/Meta";
 import theme from "@@/styles/theme";
 import "@@/styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Meta />
       <BasicMeta />
 
       <ThemeProvider theme={theme}>
